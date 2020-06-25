0a2e612937d9669772ec5edd8a6e6b5b5c68110e
simicd <10134699+simicd@users.noreply.github.com>
2020-06-25 21:30:56 +0000

    Write docstrings

--- yield-curves/yield_curves/extraction/download_yield_curve.py
+++ yield-curves/yield_curves/extraction/download_yield_curve.py
@@ -9,19 +9,38 @@ from pandas.tseries.offsets import MonthEnd
 
 
 def generate_eiopa_rfr_url(date: datetime.date):
+    """Create download URL
+
+    Args:
+        date: Yield curve date
+
+    Returns:
+        URL string
+    """
+
     base_url= r'https://www.eiopa.europa.eu/sites/default/files/risk_free_interest_rate'
     return f"{base_url}/eiopa_rfr_{str(date).replace('-', '')}.zip"
 
 
-def store_file(file_object, filepath: str):
-    """Store file at target location specified in filepath variable"""
+def store_file(file_object: bytes, filepath: str):
+    """Store file at target location specified in filepath variable
+
+    Args:
+        file_object: File as binary buffer
+        filepath: Target storage location
+    """
 
     with open(filepath, 'wb') as file:
         file.write(file_object)
 
 
-def unzip(file_object, filepath: str):
-    """Unzip file and store content at target location specified in filepath variable"""
+def unzip(file_object: BytesIO, filepath: str):
+    """Unzip file and store content at target location specified in filepath variable
+
+    Args:
+        file_object: File as BytesIO buffer
+        filepath: Target extraction location
+    """
 
     # ZipFile class expects a file location, e.g. ZipFile("file.zip","r") or a virtual file (BytesIO)
     with ZipFile(file_object) as zip_file:
@@ -29,6 +48,12 @@ def unzip(file_object, filepath: str):
 
 
 def fetch_content(url: str):
+    """Fetch content from URL and return response content if successful
+
+    Args:
+        url: Link to file download
+    """
+
     try:
         response = requests.get(url)
         if response.ok:
@@ -40,8 +65,14 @@ def fetch_content(url: str):
 
 
 def download_file(date: datetime.date, path: str):
-    """Submit response to server and return response content if successful"""
+    """Download, store and extract yield curve file for one date
 
+    Args:
+        date: Yield curve date
+        path: Target storage location
+    """
+
+    # Construct download URL from date
     url = generate_eiopa_rfr_url(date)
 
     try:
@@ -51,7 +82,9 @@ def download_file(date: datetime.date, path: str):
         if file_content is None:
             print(f"No data fetched for {date}")
         else:
-            # Since we get the file directly from the web, it's passed into BytesIO and turned into a virtual file
+            # Since the file comes as bytes array from the web,
+            # it's passed into BytesIO and turned into a virtual file
+            # This there is no need to store the .zip - only the extracted content (next step)
             file_object = BytesIO(file_content)
 
             # Unzip it into current location with subfolder
@@ -61,6 +94,12 @@ def download_file(date: datetime.date, path: str):
 
 
 def download_files(start_date: datetime.date, end_date: datetime.date, path: str):
+    """Download, store and extract yield curve files between two dates
+
+    Args:
+        date: Yield curve date
+        path: Target storage location
+    """
 
     # Use pandas functions to generate series of dates at month end between start and end date
     dates_series = date_range(start_date, end_date, freq='MS') + MonthEnd()
