f3e10d1457f0c531b918da6ca21a104d9978fee9
simicd <10134699+simicd@users.noreply.github.com>
2020-06-25 21:19:45 +0000

    Refactor download functions

--- yield-curves/main.py
+++ yield-curves/main.py
@@ -8,4 +8,4 @@ raw_data_path = r'./datasets/eiopa/raw'
 # Create folders if they don't exist
 os.makedirs(raw_data_path, exist_ok=True)
 
-download_files(start_date=date(2020, 1, 1), end_date=date(2020, 5, 1), years=[2020], path=raw_data_path)
+download_files(start_date=date(2020, 1, 1), end_date=date(2020, 5, 1), path=raw_data_path)
--- yield-curves/yield_curves/extraction/download_yield_curve.py
+++ yield-curves/yield_curves/extraction/download_yield_curve.py
@@ -28,38 +28,45 @@ def unzip(file_object, filepath: str):
         zip_file.extractall(filepath)
 
 
-def download_file(url: str):
+def fetch_content(url: str):
+    try:
+        response = requests.get(url)
+        if response.ok:
+            return response.content
+        else:
+            return None
+    except:
+        print(f"Fetching {url} not successful")
+
+
+def download_file(date: datetime.date, path: str):
     """Submit response to server and return response content if successful"""
 
-    response = requests.get(url)
-    if response.ok:
-        return response.content
-    else:
-        return None
+    url = generate_eiopa_rfr_url(date)
+
+    try:
+        # Get zipped file content from web
+        file_content = fetch_content(url)
+
+        if file_content is None:
+            print(f"No data fetched for {date}")
+        else:
+            # Since we get the file directly from the web, it's passed into BytesIO and turned into a virtual file
+            file_object = BytesIO(file_content)
+
+            # Unzip it into current location with subfolder
+            unzip(file_object, filepath=os.path.join(path, 'EIOPA-RFR', f'{date}'))
+    except:
+        print(f"Download of {date} not successful")
 
 
-def download_files(start_date: datetime.date, end_date: datetime.date, years, path):
+def download_files(start_date: datetime.date, end_date: datetime.date, path: str):
 
     # Use pandas functions to generate series of dates at month end between start and end date
     dates_series = date_range(start_date, end_date, freq='MS') + MonthEnd()
     # Convert to list of strings with format YYYY-MM-DD
     dates = dates_series.strftime("%Y-%m-%d").tolist()
 
-    # Generate URLs for each date
-    url_dict = {}
-    for date in dates:
-        url_dict[date] = generate_eiopa_rfr_url(date)
-
     # Iterate through URLs and download & unzip each of the files
-    for date, url in url_dict.items():
-        try:
-            # Get zipped file content from web
-            web_content = download_file(url)
-
-            # Since we get the file directly from the web, it's passed into BytesIO and turned into a virtual file
-            file_object = BytesIO(web_content)
-
-            # Unzip it into current location with subfolder
-            unzip(file_object, filepath=os.path.join(path, 'EIOPA-RFR', f'{date}'))
-        except:
-            print(f"Download of {date} not successful")
+    for date in dates:
+        download_file(date=date, path=path)
