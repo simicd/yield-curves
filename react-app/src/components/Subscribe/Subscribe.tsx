e000731efdd2fcb33ed43b910f89e4468a042c2f
simicd <10134699+simicd@users.noreply.github.com>
2020-07-07 21:50:23 +0200

    Capture e-mail state

--- react-app/src/components/Subscribe/Subscribe.tsx
+++ react-app/src/components/Subscribe/Subscribe.tsx
@@ -1,6 +1,8 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 export const Subscription: FC = () => {
+  const [email, setEmail] = useState("");
+
   return (
     <div className="bg-white">
       <div className="max-w-screen-xl mx-auto px-4 py-12 sm:px-6 lg:py-16 lg:px-8">
@@ -18,12 +20,16 @@ export const Subscription: FC = () => {
               <input
                 aria-label="Email address"
                 type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
                 className="appearance-none w-full px-5 py-3 border border-transparent text-base leading-6 rounded-md text-gray-900 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 transition duration-150 ease-in-out"
                 placeholder="Enter your email"
               />
               <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
-                <button className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-900 border-white hover:bg-indigo-400 focus:outline-none focus:bg-indigo-400 transition duration-150 ease-in-out">
+                <button
+                  className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-900 border-white hover:bg-indigo-400 focus:outline-none focus:bg-indigo-400 transition duration-150 ease-in-out"
+                  onClick={() => window.alert("This is a demo - the e-mail address you entered is \n" + email)}>
                   Notify me
                 </button>
               </div>
