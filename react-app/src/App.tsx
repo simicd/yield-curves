ac2cef41a75ac500ab8b823b597924310f0034ab
simicd <10134699+simicd@users.noreply.github.com>
2020-07-08 20:19:27 +0200

    Create pricing component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Home } from "./views/Home";
 import "./App.css";
 import { Subscription } from "./components/Subscribe/Subscribe";
+import { Pricing } from "./components/Pricing/Pricing";
 
 function App() {
   return (
     <>
       <Home />
+      <Pricing></Pricing>
       <Subscription></Subscription>
     </>
   );
