bdb1f65c063b5cd185af5bacdfea169142bfcb58
simicd <10134699+simicd@users.noreply.github.com>
2020-07-08 20:49:45 +0200

    Parametrize pricing cards

--- react-app/src/components/Pricing/Pricing.tsx
+++ react-app/src/components/Pricing/Pricing.tsx
@@ -15,9 +15,33 @@ export const Pricing: FC = () => {
             <div className="absolute inset-0 bg-gray-900 h-5/6 lg:h-2/3"></div>
             <div className="max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
               <div className="relative lg:grid lg:grid-cols-7">
-                <PricingCardBudget />
-                <PricingCardMain />
-                <PricingCardTop />
+                <PricingCardBudget
+                  title="Hobby"
+                  amount={79}
+                  benefits={["Pariatur quod similique", "Sapiente libero doloribus", "Vel ipsa esse repudiandae"]}
+                  buttonText="Show dashboard"
+                  buttonUrl="/"
+                />
+                <PricingCardMain
+                  title="Growth"
+                  amount={149}
+                  benefits={[
+                    "Quia rem est sed impedit magnam",
+                    "Dolorem vero ratione voluptates",
+                    "Qui sed ab doloribus voluptatem dolore",
+                    "Laborum commodi molestiae id et fugiat",
+                    "Nam ut ipsa nesciunt culpa modi dolor",
+                  ]}
+                  buttonText="Show dashboard"
+                  buttonUrl="/"
+                />
+                <PricingCardTop
+                  title="Scale"
+                  amount={349}
+                  benefits={["Pariatur quod similique", "Sapiente libero doloribus", "Vel ipsa esse repudiandae"]}
+                  buttonText="Show dashboard"
+                  buttonUrl="/"
+                />
               </div>
             </div>
           </div>
@@ -35,7 +59,15 @@ const CheckmarkIcon: FC = () => {
   );
 };
 
-const PricingCardTop: FC = () => {
+interface PricingCardProps {
+  title: string;
+  amount: number;
+  benefits: string[];
+  buttonText: string;
+  buttonUrl: string;
+}
+
+const PricingCardTop: FC<PricingCardProps> = ({ title, amount, benefits, buttonText, buttonUrl }) => {
   return (
     <div className="max-w-md mx-auto mt-10 lg:m-0 lg:max-w-none lg:col-start-6 lg:col-end-8 lg:row-start-2 lg:row-end-3">
       <div className="flex flex-col h-full overflow-hidden rounded-lg shadow-lg lg:rounded-none lg:rounded-r-lg">
@@ -43,12 +75,12 @@ const PricingCardTop: FC = () => {
           <div className="px-6 py-10 bg-white">
             <div>
               <h3 className="text-2xl font-medium leading-8 text-center text-gray-900" id="tier-scale">
-                Scale
+                {title}
               </h3>
               <div className="flex items-center justify-center mt-4">
                 <span className="flex items-start px-3 text-6xl leading-none tracking-tight text-gray-900">
                   <span className="mt-2 mr-2 text-4xl font-medium">$</span>
-                  <span className="font-extrabold">349</span>
+                  <span className="font-extrabold">{amount}</span>
                 </span>
                 <span className="text-xl font-medium leading-7 text-gray-500">/month</span>
               </div>
@@ -56,32 +88,22 @@ const PricingCardTop: FC = () => {
           </div>
           <div className="flex flex-col justify-between flex-1 p-6 border-t-2 border-gray-100 bg-gray-50 sm:p-10 lg:p-6 xl:p-10">
             <ul>
-              <li className="flex items-start">
-                <div className="flex-shrink-0">
-                  <CheckmarkIcon />
-                </div>
-                <p className="ml-3 text-base font-medium leading-6 text-gray-500">Pariatur quod similique</p>
-              </li>
-              <li className="flex items-start mt-4">
-                <div className="flex-shrink-0">
-                  <CheckmarkIcon />
-                </div>
-                <p className="ml-3 text-base font-medium leading-6 text-gray-500">Sapiente libero doloribus</p>
-              </li>
-              <li className="flex items-start mt-4">
-                <div className="flex-shrink-0">
-                  <CheckmarkIcon />
-                </div>
-                <p className="ml-3 text-base font-medium leading-6 text-gray-500">Vel ipsa esse repudiandae</p>
-              </li>
+              {benefits.map((benefit) => (
+                <li className="flex items-start mt-4">
+                  <div className="flex-shrink-0">
+                    <CheckmarkIcon />
+                  </div>
+                  <p className="ml-3 text-base font-medium leading-6 text-gray-500">{benefit}</p>
+                </li>
+              ))}
             </ul>
             <div className="mt-8">
               <div className="rounded-lg shadow-md">
                 <a
-                  href="#"
+                  href={buttonUrl}
                   className="block w-full px-6 py-3 text-base font-medium leading-6 text-center text-indigo-600 transition duration-150 ease-in-out bg-white border border-transparent rounded-lg hover:text-indigo-500 focus:outline-none focus:shadow-outline"
                   aria-describedby="tier-scale">
-                  Start your trial
+                  {buttonText}
                 </a>
               </div>
             </div>
@@ -92,7 +114,7 @@ const PricingCardTop: FC = () => {
   );
 };
 
-const PricingCardMain: FC = () => {
+const PricingCardMain: FC<PricingCardProps> = ({ title, amount, benefits, buttonText, buttonUrl }) => {
   return (
     <div className="max-w-lg mx-auto mt-10 lg:mt-0 lg:max-w-none lg:mx-0 lg:col-start-3 lg:col-end-6 lg:row-start-1 lg:row-end-4">
       <div className="relative z-10 rounded-lg shadow-xl">
@@ -107,12 +129,12 @@ const PricingCardMain: FC = () => {
         <div className="px-6 pt-12 pb-10 bg-white rounded-t-lg">
           <div>
             <h3 className="text-3xl font-semibold leading-9 text-center text-gray-900 sm:-mx-6" id="tier-growth">
-              Growth
+              {title}
             </h3>
             <div className="flex items-center justify-center mt-4">
               <span className="flex items-start px-3 text-6xl leading-none tracking-tight text-gray-900 sm:text-6xl">
                 <span className="mt-2 mr-2 text-4xl font-medium">$</span>
-                <span className="font-extrabold">149</span>
+                <span className="font-extrabold">{amount}</span>
               </span>
               <span className="text-2xl font-medium leading-8 text-gray-500">/month</span>
             </div>
@@ -120,50 +142,22 @@ const PricingCardMain: FC = () => {
         </div>
         <div className="px-6 pt-10 pb-8 border-t-2 border-gray-100 rounded-b-lg bg-gray-50 sm:px-10 sm:py-10">
           <ul>
-            <li className="flex items-start">
-              <div className="flex-shrink-0">
-                <CheckmarkIcon />
-              </div>
-              <p className="ml-3 text-base font-medium leading-6 text-gray-500">Quia rem est sed impedit magnam</p>
-            </li>
-            <li className="flex items-start mt-4">
-              <div className="flex-shrink-0">
-                <CheckmarkIcon />
-              </div>
-              <p className="ml-3 text-base font-medium leading-6 text-gray-500">Dolorem vero ratione voluptates</p>
-            </li>
-            <li className="flex items-start mt-4">
-              <div className="flex-shrink-0">
-                <CheckmarkIcon />
-              </div>
-              <p className="ml-3 text-base font-medium leading-6 text-gray-500">
-                Qui sed ab doloribus voluptatem dolore
-              </p>
-            </li>
-            <li className="flex items-start mt-4">
-              <div className="flex-shrink-0">
-                <CheckmarkIcon />
-              </div>
-              <p className="ml-3 text-base font-medium leading-6 text-gray-500">
-                Laborum commodi molestiae id et fugiat
-              </p>
-            </li>
-            <li className="flex items-start mt-4">
-              <div className="flex-shrink-0">
-                <CheckmarkIcon />
-              </div>
-              <p className="ml-3 text-base font-medium leading-6 text-gray-500">
-                Nam ut ipsa nesciunt culpa modi dolor
-              </p>
-            </li>
+            {benefits.map((benefit) => (
+              <li className="flex items-start mt-4">
+                <div className="flex-shrink-0">
+                  <CheckmarkIcon />
+                </div>
+                <p className="ml-3 text-base font-medium leading-6 text-gray-500">{benefit}</p>
+              </li>
+            ))}
           </ul>
           <div className="mt-10">
             <div className="rounded-lg shadow-md">
               <a
-                href="#"
+                href={buttonUrl}
                 className="block w-full px-6 py-4 text-xl font-medium leading-6 text-center text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-lg hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo"
                 aria-describedby="tier-growth">
-                Start your trial
+                {buttonText}
               </a>
             </div>
           </div>
@@ -173,7 +167,7 @@ const PricingCardMain: FC = () => {
   );
 };
 
-const PricingCardBudget: FC = () => {
+const PricingCardBudget: FC<PricingCardProps> = ({ title, amount, benefits, buttonText, buttonUrl }) => {
   return (
     <div className="max-w-md mx-auto lg:mx-0 lg:max-w-none lg:col-start-1 lg:col-end-3 lg:row-start-2 lg:row-end-3">
       <div className="flex flex-col h-full overflow-hidden rounded-lg shadow-lg lg:rounded-none lg:rounded-l-lg">
@@ -181,12 +175,12 @@ const PricingCardBudget: FC = () => {
           <div className="px-6 py-10 bg-white">
             <div>
               <h3 className="text-2xl font-medium leading-8 text-center text-gray-900" id="tier-hobby">
-                Hobby
+                {title}
               </h3>
               <div className="flex items-center justify-center mt-4">
                 <span className="flex items-start px-3 text-6xl leading-none tracking-tight text-gray-900">
                   <span className="mt-2 mr-2 text-4xl font-medium">$</span>
-                  <span className="font-extrabold">79</span>
+                  <span className="font-extrabold">{amount}</span>
                 </span>
                 <span className="text-xl font-medium leading-7 text-gray-500">/month</span>
               </div>
@@ -194,32 +188,22 @@ const PricingCardBudget: FC = () => {
           </div>
           <div className="flex flex-col justify-between flex-1 p-6 border-t-2 border-gray-100 bg-gray-50 sm:p-10 lg:p-6 xl:p-10">
             <ul>
-              <li className="flex items-start">
-                <div className="flex-shrink-0">
-                  <CheckmarkIcon />
-                </div>
-                <p className="ml-3 text-base font-medium leading-6 text-gray-500">Pariatur quod similique</p>
-              </li>
-              <li className="flex items-start mt-4">
-                <div className="flex-shrink-0">
-                  <CheckmarkIcon />
-                </div>
-                <p className="ml-3 text-base font-medium leading-6 text-gray-500">Sapiente libero doloribus</p>
-              </li>
-              <li className="flex items-start mt-4">
-                <div className="flex-shrink-0">
-                  <CheckmarkIcon />
-                </div>
-                <p className="ml-3 text-base font-medium leading-6 text-gray-500">Vel ipsa esse repudiandae</p>
-              </li>
+              {benefits.map((benefit) => (
+                <li className="flex items-start mt-4">
+                  <div className="flex-shrink-0">
+                    <CheckmarkIcon />
+                  </div>
+                  <p className="ml-3 text-base font-medium leading-6 text-gray-500">{benefit}</p>
+                </li>
+              ))}
             </ul>
             <div className="mt-8">
               <div className="rounded-lg shadow-md">
                 <a
-                  href="#"
+                  href={buttonUrl}
                   className="block w-full px-6 py-3 text-base font-medium leading-6 text-center text-indigo-600 transition duration-150 ease-in-out bg-white border border-transparent rounded-lg hover:text-indigo-500 focus:outline-none focus:shadow-outline"
                   aria-describedby="tier-hobby">
-                  Start your trial
+                  {buttonText}
                 </a>
               </div>
             </div>
