c50f87abb055eecb490d21f69ae754f4219439a4
fyyying <45222861+fyyying@users.noreply.github.com>
2020-07-09 18:57:21 +0000

    Add the test stripe payment files

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -5,6 +5,18 @@ import "./App.css";
 import { Playground } from "./views/Playground";
 import { Layout } from "./components/Layout/Layout";
 
+import { loadStripe } from "@stripe/stripe-js";
+
+import { Elements } from "@stripe/react-stripe-js";
+
+import CheckoutForm from "./views/Checkout/CheckoutForm";
+
+import "./views/Checkout/Checkout.css";
+
+const promise = loadStripe(
+  "pk_test_51H2hX9BT829BW03KzheY9QfICpGEzuUDvDqaa5Tgnhyu0hpyMkdh6J6DbU5O6qHRjQD3b8j4L9GS8CPiOOKDzmio00L6NYrRBa"
+);
+
 function App() {
   return (
     <>
@@ -12,6 +24,12 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/playground" component={Playground} />
       </Layout>
+      <Route exact path="/" component={Home} />
+      {/* <Home />
+      <Subscription></Subscription> */}
+        <Elements stripe={promise}>
+          <CheckoutForm />
+        </Elements>
     </>
   );
 }
