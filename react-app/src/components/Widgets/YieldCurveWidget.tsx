e9b83229d54516fc0a0fa5f5153fdb20f7cf3f41
simicd <10134699+simicd@users.noreply.github.com>
2020-07-13 22:22:59 +0200

    Customize yield curve widget

--- react-app/src/components/Widgets/YieldCurveWidget.tsx
+++ react-app/src/components/Widgets/YieldCurveWidget.tsx
@@ -1,6 +1,5 @@
 import React, { FC, useState } from "react";
-import { LineChart } from "../Charts/LineChart";
-import { Serie } from "@nivo/line";
+import { Serie, ResponsiveLine } from "@nivo/line";
 import clsx from "clsx";
 
 interface YieldCurveWidgetProps {
@@ -12,7 +11,7 @@ export const YieldCurveWidget: FC<YieldCurveWidgetProps> = ({ data }) => {
 
   return (
     <>
-      <div style={{ width: 800, height: 600 }} className="mx-auto mt-8">
+      <div style={{ width: 800, height: 500 }} className="mx-auto mt-8">
         <h3 className="mx-6 text-lg font-semibold tracking-wider text-indigo-600 uppercase">
           Yield curves <span className="text-xs text-gray-400">June 30th, 2020</span>
         </h3>
@@ -28,7 +27,64 @@ export const YieldCurveWidget: FC<YieldCurveWidgetProps> = ({ data }) => {
             </button>
           ))}
         </div>
-        <LineChart data={data.filter((d) => d.id === country)}></LineChart>
+
+        <ResponsiveLine
+          data={data.filter((d) => d.id === country)}
+          margin={{ top: 50, right: 60, bottom: 100, left: 60 }}
+          xScale={{ type: "linear" }}
+          yScale={{ type: "linear", min: -0.01, max: 0.05 }}
+          yFormat=".1%"
+          curve="natural"
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+            orient: "bottom",
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "Time to maturity",
+            legendOffset: 36,
+            legendPosition: "middle",
+          }}
+          axisLeft={{
+            orient: "left",
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "Interest rate",
+            legendOffset: -50,
+            legendPosition: "middle",
+            format: ".1%",
+            tickValues: 6,
+          }}
+          enableGridX={false}
+          gridYValues={6}
+          enableSlices="x"
+          colors={"#5145cd"}
+          enablePoints={true}
+          pointColor={{ from: 'color', modifiers: [] }}
+          pointBorderWidth={2}
+          pointBorderColor={{ from: 'color', modifiers: [] }}
+          pointLabel="y"
+          pointLabelYOffset={-12}
+          crosshairType="bottom"
+          useMesh={true}
+          enableArea={true}
+          defs={[
+            {
+              id: "gradient",
+              type: "linearGradient",
+              colors: [
+                { offset: 0, color: "inherit" },
+                { offset: 100, color: "white" },
+              ],
+            },
+          ]}
+          fill={[
+            // match any object
+            { match: "*", id: "gradient" },
+          ]}
+        />
       </div>
     </>
   );
