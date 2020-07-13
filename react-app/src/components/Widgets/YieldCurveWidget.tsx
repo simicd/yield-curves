1412fe5cdb65c409dab85874c4874e80c2667b7b
simicd <10134699+simicd@users.noreply.github.com>
2020-07-13 21:44:15 +0200

    Add selection buttons to yield curve widget

--- react-app/src/components/Widgets/YieldCurveWidget.tsx
+++ react-app/src/components/Widgets/YieldCurveWidget.tsx
@@ -1,19 +1,34 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { LineChart } from "../Charts/LineChart";
 import { Serie } from "@nivo/line";
+import clsx from "clsx";
 
 interface YieldCurveWidgetProps {
   data: Serie[];
 }
 
 export const YieldCurveWidget: FC<YieldCurveWidgetProps> = ({ data }) => {
+  const [country, setCountry] = useState(data[0].id);
+
   return (
     <>
       <div style={{ width: 800, height: 600 }} className="mx-auto mt-8">
         <h3 className="mx-6 text-lg font-semibold tracking-wider text-indigo-600 uppercase">
           Yield curves <span className="text-xs text-gray-400">June 30th, 2020</span>
         </h3>
-        <LineChart data={data}></LineChart>
+        <div className="ml-6">
+          {data.map((d) => (
+            <button
+              className={clsx("p-2 mx-1 my-2 text-white rounded-md hover:bg-indigo-400", {
+                "bg-indigo-400": country === d.id,
+                "bg-indigo-700": country !== d.id,
+              })}
+              onClick={() => setCountry(d.id)}>
+              {d.id}
+            </button>
+          ))}
+        </div>
+        <LineChart data={data.filter((d) => d.id === country)}></LineChart>
       </div>
     </>
   );
