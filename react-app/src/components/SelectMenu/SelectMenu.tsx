7ec5de2e63102c92520cee0ba64564a6197ee840
fyyying <45222861+fyyying@users.noreply.github.com>
2020-07-28 11:02:07 +0000

    Add structured data into index.html

--- react-app/src/components/SelectMenu/SelectMenu.tsx
+++ react-app/src/components/SelectMenu/SelectMenu.tsx
@@ -72,7 +72,7 @@ export const SelectMenu: FC = () => {
     <div className="bg-gray-50">
       <div className="left-0 max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-3xl sm:leading-10">
-          Download cleaned risk free EIOPA rates here:
+          Download risk free EIOPA rates <br/>between 2016 and the most recent month
           <br />
           <span className="text-teal-500">Pick a month and currency</span>
         </h2>
