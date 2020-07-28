3d3c2f42b2bc8ef0bd852be26dc6e5e0d247d339
simicd <10134699+simicd@users.noreply.github.com>
2020-07-28 22:05:58 +0000

    Refactor features into list and list item components

---
+++ react-app/src/components/Feature/FeatureList.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react";
+
+
+/**
+ * List of features displayed as two-column grid (medium screens larger) or single column (smaller screens)
+ * @param props Component children
+ */
+export const FeatureList: FC = ({ children }) => {
+  return (
+    <div className="mt-10">
+      <ul className="md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
+        {children}
+      </ul>
+    </div>
+  );
+};
---
+++ react-app/src/components/Feature/FeatureListItem.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from "react";
+
+interface FeatureListItemProps {
+  /** Feature title */
+  title: string;
+  /** Feature description */
+  description: string;
+  /** SVG icon */
+  svgPath: React.ReactNode;
+}
+
+/**
+ * List items with svg icon and a bold title plus light description - works well with the FeatureList component
+ * @param props Title, description and svgPath
+ */
+export const FeatureListItem: FC<FeatureListItemProps> = ({ title, description, svgPath }) => {
+  return (
+    <>
+      <li className="mt-10 md:mt-0">
+        <div className="flex lg:ml-1/4">
+          <div className="flex-shrink-0">
+            <div className="flex items-center justify-center w-12 h-12 text-white bg-teal-600 rounded-md">
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                {svgPath}
+              </svg>
+            </div>
+          </div>
+          <div className="ml-4">
+            <h4 className="text-lg font-medium leading-6 text-gray-900">{title}</h4>
+            <p className="mt-2 text-base leading-6 text-gray-500">{description}</p>
+          </div>
+        </div>
+      </li>
+    </>
+  );
+};
