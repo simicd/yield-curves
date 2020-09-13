26fbf192f0aa6e95c7b07535bf41c86a381df336
simicd <10134699+simicd@users.noreply.github.com>
2020-09-13 21:03:17 +0200

    Upate tailwindcss

--- react-app/src/components/Button/Button.tsx
+++ react-app/src/components/Button/Button.tsx
@@ -17,7 +17,7 @@ export const Button: FC<ButtonProps> = ({ size = "md", theme = "primary", onClic
             "px-4 py-2 text-sm leading-5": size === "sm",
             "px-6 py-3 text-base leading-6": size === "md",
             "px-10 py-4 text-lg font-medium leading-7": size === "lg",
-            "text-white bg-teal-600 bg-gradient-to-tr from-teal-500 to-blue-800 hover:text-white hover:from-teal-400 hover:to-blue-700 focus:from-teal-400 focus:to-blue-700": theme === "primary",
+            "text-white bg-teal-600 bg-gradient-to-tr from-teal-500 to-blue-800 hover:from-teal-400 hover:to-blue-700 focus:from-teal-400 focus:to-blue-700": theme === "primary",
             "text-teal-800 bg-cool-gray-100 hover:text-teal-500 hover:bg-cool-gray-100 focus:bg-cool-gray-100":
               theme === "light",
             "text-white bg-transparent hover:text-white hover:bg-teal-500 focus:bg-teal-500 border-white":
